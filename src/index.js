073ff09e795aa659477185e621036db8443eb880
Mohammad <mohammad@gmail.com>
2026-02-26 13:20:36 +0200

    Update welcome message

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('Notely');
+res.end('Welcome to Notely');
 });
 
 const port = process.env.PORT || 8080;
